6b88c1aaf16f86a75d79239d8570a027a927bd42
Angela Villa <ajvr2512@gmail.com>
2023-03-19 14:37:24 -0500

    Update Location.jsx

--- src/component/Location.jsx
+++ src/component/Location.jsx
@@ -3,7 +3,7 @@ import React from 'react';
 const Location = ({ name, type, dimension, residents }) => {
   return (
     <div className="bg-[#062226] text-white">
-      <h2 className="flex justify-center text-[35px]">{name}</h2>
+      <h2 className="NameLocation flex justify-center text-[35px]">{name}</h2>
       <ul className="ContainerLocation">
         <li>
           <span>Type: </span>
